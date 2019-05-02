bb1fefb09da966d36b2b112ac8070ab1702e3b02
Michael Redig <mredig@gmail.com>
2019-05-02 12:14:19 -0500

    added classes for collectionviews and connected the cells' outlets

---
+++ Planets/View Contoller/PlanetCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PlanetCollectionViewCell.swift
+//  Planets
+//
+//  Created by Michael Redig on 5/2/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class PlanetCollectionViewCell: UICollectionViewCell {
+	@IBOutlet var planetImageView: UIImageView!
+	@IBOutlet var planetLabel: UILabel!
+	
+	
+}
